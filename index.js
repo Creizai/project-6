cc1b31b4b51afd914ee12d35f7ea01d4d4cb6dd0
Adam_Bates <Creizai@EMAIL.COM>
2020-01-02 16:23:16 -0500

    Added create funcionality but returning errors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const quotesRoutes = require("./lib/routes/quotes.js");
-const quotesControllers = require("./lib/controllers/quotes.js");
+var parser = require("body-parser");
 
 // When user requests at the root / of our application, .use
 app.use("/", quotesRoutes);
+
+app.use(parser.json());
 
 app.listen(8081, () => console.log("Port 8080, reporting for duty sir."));
