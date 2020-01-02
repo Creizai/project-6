f3581c9f06fa587908aa1d394754c652eb957d42
Adam_Bates <Creizai@EMAIL.COM>
2020-01-02 18:39:19 -0500

    in index js Moved app use parser ABOVE app use routes to FINALLY FIX THE PROBLEM

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const app = express();
 const quotesRoutes = require("./lib/routes/quotes.js");
 var parser = require("body-parser");
 let port = 8081;
+
+// URL ENCODER
+app.use(parser.urlencoded({ extended: true }));
+
+// MUST PARSE BEFORE USING THE ROUTE
+app.use(parser.json());
+
 // When user requests at the root / of our application, .use
 app.use("/", quotesRoutes);
-
-app.use(parser.json());
 
 app.listen(port, () =>
   console.log(`Listening on port ${port}, reporting for duty sir.`)
