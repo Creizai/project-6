d3a2173d501d6ce17e911106b63c7483b2df1a41
Adam_Bates <Creizai@EMAIL.COM>
2020-01-02 17:57:18 -0500

    stuck on postman error

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const quotesRoutes = require("./lib/routes/quotes.js");
 var parser = require("body-parser");
-
+let port = 8081;
 // When user requests at the root / of our application, .use
 app.use("/", quotesRoutes);
 
 app.use(parser.json());
 
-app.listen(8081, () => console.log("Port 8080, reporting for duty sir."));
+app.listen(port, () =>
+  console.log(`Listening on port ${port}, reporting for duty sir.`)
+);
