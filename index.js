d238eefa48d484de22f79dcf5a07c012c57484ce
Adam_Bates <Creizai@EMAIL.COM>
2020-01-02 14:40:24 -0500

    Moved the routes folder to fix a location error

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const quotesRoutes = require("./lib/routes/routes.js");
-
-app.listen(8081, () => console.log("Port 8080, reporting for duty sir."));
+const quotesRoutes = require("./lib/routes/quotes.js");
 
 // When user requests at the root / of our application, .use
 app.use("/", quotesRoutes);
+
+app.listen(8081, () => console.log("Port 8080, reporting for duty sir."));
